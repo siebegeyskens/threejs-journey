55768627c45429eb89696c3ff58c6612beff598e
Siebe Geyskens <siebe.geyskens@student.ehb.be>
2023-02-16 18:40:15 +0100

    create Experience component

---
+++ 42-first-r3f-application/src/index.jsx
@@ -0,0 +1,14 @@
+import "./style.css";
+import ReactDOM from "react-dom/client";
+import { Canvas } from "@react-three/fiber";
+import Experience from "./Experience.jsx";
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+root.render(
+  <>
+    <Canvas>
+      <Experience></Experience>
+    </Canvas>
+  </>
+);
